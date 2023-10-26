1252b225c96d2d0f1bc13ee5e90c63a323e25ffc
PalwanMusa <tacmammedowmusa657@gmail.com>
2023-10-26 17:05:30 +0500

    Finished Project

--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {BsFillTrash2Fill} from "react-icons/bs"
 import "./List.css"
 
 const List = () => {
 
-    const cars = [ "Audi" , "BMW" , "Mercedes" , "Camry" , "Toyota" , "Honda" , "Hundai" , 'Ford']
+    const [cars, setCars] = useState ([ "Audi" , "BMW" , "Mercedes" , "Camry" , "Toyota" , "Honda" , "Hundai" , 'Ford'])
 
+    const [value,setValue] = useState("")
+
+    const changeHandler = (event) => {
+      setValue(event.target.value)
+    }
+    
+    const addCarHandler = () => {
+      if (value === ""){
+        return alert("Bosh Yazdynyz")
+      }
+      if (!cars.includes(value)) {
+      const newCars = [value , ...cars]
+      setCars(newCars)
+      setValue("")
+      } else {
+        alert("Yazan masynynyz ozal sanawa girizilen , bashga at yazmagagynyzy hayys edyaris!")
+      }
+    }
+    const deleteHandler = (carName) => {
+      const deletedCarList = cars.filter((car) => car !== carName)
+      setCars(deletedCarList)
+    }
   return (
     <div className='list__container'>
-        <h2>List Of Cars</h2>
+
+        <div className='addCar'>
+        <h2>Add a new car</h2>
+        <input value={value} onChange={changeHandler} placeholder='car name' type='text' />
+        <br />
+        <button onClick={addCarHandler}>Add</button>
+        </div>
+        <h1>List Of Cars</h1>
+
         <ul>{cars.map(carName => {
-            return <li key={carName}>{carName}</li>
+            return <li key={carName}>{carName}
+            <BsFillTrash2Fill onClick={() => deleteHandler(carName)} className='list-icon' />
+            </li>
         })}</ul>
     </div>
   )
